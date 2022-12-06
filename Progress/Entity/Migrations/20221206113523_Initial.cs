b5ec2b438c0c94f2316952805364848634ab2273
Polina Efimova <polina.efimova@onlineinterviewer.ru>
2022-12-06 14:37:42 +0300

    добавила модели (первоначальные, далее будем дорабатывать), создала миграцию

---
+++ Progress/Entity/Migrations/20221206113523_Initial.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "data");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartGame = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsGameOver = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IpAdress = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BattelFields",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BattelFields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BattelFields_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "data",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameStatistics",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GameId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    IsWin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameStatistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameStatistics_Games_GameId",
+                        column: x => x.GameId,
+                        principalSchema: "data",
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameStatistics_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "data",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BattelFields_UserId",
+                schema: "data",
+                table: "BattelFields",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStatistics_GameId",
+                schema: "data",
+                table: "GameStatistics",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStatistics_UserId",
+                schema: "data",
+                table: "GameStatistics",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BattelFields",
+                schema: "data");
+
+            migrationBuilder.DropTable(
+                name: "GameStatistics",
+                schema: "data");
+
+            migrationBuilder.DropTable(
+                name: "Games",
+                schema: "data");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "data");
+        }
+    }
+}
---
+++ Progress/Entity/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace Entity.Models;
+
+[Table("Games", Schema = "data")]
+public class Game
+{
+    [Key]
+    public long Id { get; set; }
+    public DateTime StartGame { get; set; }
+    public bool IsGameOver { get; set; }
+}
