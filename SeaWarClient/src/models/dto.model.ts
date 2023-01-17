a0c126347a7f73099debe24c3ee65ad2c613d1c2
Степанов Алексей <bssclient@gmail.com>
2023-01-17 17:51:15 +0500

    обавил обработку команд на выстрел

--- SeaWarClient/src/models/dto.model.ts
+++ SeaWarClient/src/models/dto.model.ts
@@ -10,3 +10,22 @@ export interface IMessageDto<T> {
 export interface IShipDto {
   coordinates: Array<ICoordinateSimple>;
 }
+
+export interface IInitGameDto {
+  otherGamerConnectionId: string;
+  shootGamerConnectionId: string;
+}
+export enum EShootStatus {
+  Error = -1,
+  Unknown = 0,
+  Miss = 1,
+  Hit = 2,
+  Killing = 3,
+  KillingAll = 4,
+}
+
+export interface ShootResultDto {
+  shootStatus: EShootStatus;
+  sourceGamerConnectionId: string;
+  targetGamerConnectionId: string;
+}
--- SeaWarClient/src/stores/gameStore.ts
+++ SeaWarClient/src/stores/gameStore.ts
@@ -1,59 +1,11 @@
-import { defineStore } from "pinia";
 import {
-  HubConnectionBuilder,
-  HttpTransportType,
-  HubConnection,
-} from "@microsoft/signalr";
-import type { IMessageDto, IShipDto } from "@/models/dto.model";
-import { v4 as uuidv4 } from "uuid";
-import { filter, map, Subject, take } from "rxjs";
-
-class WsWorker {
-  reciveMsg$ = new Subject<IMessageDto<any>>();
-  connection: HubConnection;
-
-  constructor(url: string) {
-    this.connection = new HubConnectionBuilder()
-      .withUrl(url, {
-        skipNegotiation: true,
-        transport: HttpTransportType.WebSockets,
-      })
-      .build();
-
-    this.connection.start().then(() => {
-      console.log("соединение по WS установленно");
-      this.subscribeMsg();
-    });
-    this.reciveMsg$.subscribe((msg) => {
-      console.log("от сервера: ", msg);
-    });
-  }
-
-  sendMsg<T>(commandName: string, payload: T) {
-    const msgUid = uuidv4();
-    this.connection.invoke(commandName, <IMessageDto<T>>{
-      uid: msgUid,
-      payload,
-    });
-    return msgUid;
-  }
-
-  getAnswer$<T>(msgUid: string) {
-    return this.reciveMsg$.pipe(
-      filter((payload) => payload.uid === msgUid),
-      take(1),
-      map((payload) => payload.payload as T)
-    );
-  }
-
-  private subscribeMsg() {
-    this.connection.on("Responce", (data: IMessageDto<any>) => {
-      this.reciveMsg$.next(data);
-    });
-  }
-}
-
-const wsWorker = new WsWorker("http://localhost:5127/gameHub");
+  type IShipDto,
+  type ICoordinateSimple,
+  EShootStatus,
+} from "@/models/dto.model";
+import { defineStore } from "pinia";
+import { filter, switchMap, take, tap, map } from "rxjs";
+import { wsWorker } from "./wsWorker.service";
 
 export enum EStatusGame {
   Init = "Init",
@@ -65,6 +17,8 @@ export const gameStore = defineStore("game", {
   state() {
     return {
       gameStatus: EStatusGame.Init,
+      otherGamerId: null as string,
+      shootGamerId: null as string,
     };
   },
   actions: {
@@ -72,11 +26,46 @@ export const gameStore = defineStore("game", {
     gameStart(sheeps: Array<IShipDto>) {
       wsWorker
         .getAnswer$<boolean>(wsWorker.sendMsg("GameStart", sheeps))
+        .pipe(
+          filter((result) => result),
+          tap(() => (this.gameStatus = EStatusGame.Find)),
+          switchMap(() =>
+            wsWorker.startGame$.pipe(
+              take(1),
+              tap((result) => {
+                this.otherGamerId = result.otherGamerConnectionId;
+                this.shootGamerId = result.shootGamerConnectionId;
+                this.gameStatus = EStatusGame.Game;
+              })
+            )
+          ),
+          switchMap(() => wsWorker.resultShoot$),
+          map((msg) => msg.payload)
+        )
         .subscribe((result) => {
-          if (result) {
-            this.gameStatus = EStatusGame.Find;
+          if (
+            [
+              EShootStatus.Hit,
+              EShootStatus.Killing,
+              EShootStatus.KillingAll,
+            ].includes(result.shootStatus)
+          ) {
+            this.shootGamerId = result.sourceGamerConnectionId;
+            return;
           }
+          this.shootGamerId = result.targetGamerConnectionId;
         });
+    },
+    sendShoot(coordinate: ICoordinateSimple) {
+      wsWorker.sendMsg("Shoot", coordinate);
+    },
+  },
+  getters: {
+    connectionId() {
+      return wsWorker.connectionId$.value;
+    },
+    isShoot(state) {
+      return wsWorker.connectionId$.value === state.shootGamerId;
     },
   },
 });
