383a2ed4976262db3ba167c07f7bbbf46766d4ac
VasiliyDudin <117008684+VasiliyDudin@users.noreply.github.com>
2023-03-07 00:14:32 +0300

    Add files via upload

---
+++ Progress/View/Models/Out/OutUserStatistics.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Models.Out
+{
+    public class OutUserStatistics
+    {
+        public long UserId { get; set; }
+
+        public Level Level { get; set; }
+
+        public int Rating { get; set; }
+
+        public bool IsPrivileged { get; set; }
+
+        public int GameCount { get; set; }
+
+        public int WinGames { get; set; }
+        public int LossGames { get; set; }
+    }
+}
