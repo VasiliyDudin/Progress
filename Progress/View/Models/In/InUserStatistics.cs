204e9550da7910c9f59c6ced800997aa5de6e3e2
VasiliyDudin <117008684+VasiliyDudin@users.noreply.github.com>
2023-03-07 00:13:48 +0300

    Add files via upload

---
+++ Progress/View/Models/In/InUserStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Models.In
+{
+    public class InUserStatistics
+    {
+        public long UserId { get; set; }
+        public int GameCount { get; set; }
+
+        public int WinGames { get; set; }
+        public int LossGames { get; set; }
+    }
+}
