9ebbe274354da40e5bcb7b7daaa10fb534772b95
Polina Efimova <polina.efimova@onlineinterviewer.ru>
2023-03-07 17:07:16 +0300

    миграция UserStatistic

---
+++ Progress/Entity/Migrations/20230307140041_statistics.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class statistics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.CreateTable(
+                name: "UserStatistic",
+                schema: "user",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    userid = table.Column<long>(type: "bigint", nullable: false),
+                    level = table.Column<int>(type: "integer", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: false),
+                    isprivileged = table.Column<bool>(type: "boolean", nullable: false),
+                    gamecount = table.Column<int>(type: "integer", nullable: false),
+                    wingames = table.Column<int>(type: "integer", nullable: false),
+                    lossgames = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStatistic", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserStatistic",
+                schema: "user");
+        }
+    }
+}
